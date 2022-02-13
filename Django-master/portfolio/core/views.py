f1f625746299437d78b0217f65d5a31870ad9caf
TIMMMYYYHHH <76937640+TIMMMYYYHHH@users.noreply.github.com>
2022-02-13 21:52:57 +0200

    Ui fix
    Update many ui bugs

--- Django-master/portfolio/core/views.py
+++ Django-master/portfolio/core/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from project.models import Post
 
 def indexView(request):
-  posts = Post.objects.all()
+  posts = Post.objects.all()[0:3]
   return render(request, 'core/index.html', {'posts':posts})
 
 
 def projectView(request):
   posts = Post.objects.all()
-  return render(request, 'core/project.html', {'posts':posts})
+  latestPosts = Post.objects.all()[0:3]
+  return render(request, 'core/project.html', {'posts':posts, 'latestPosts':latestPosts})
 
 def aboutView(request):
   return render(request, 'core/about.html')
--- Django-master/portfolio/project/views.py
+++ Django-master/portfolio/project/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from project.models import Post
 
-
-from .models import Post
 
 def detailView(request, slug):
   post = get_object_or_404(Post, slug=slug)
-
-  return render(request,'project/detail.html', {'post': post})
+  latestPosts = Post.objects.all()[0:3]
+  return render(request,'project/detail.html', {'post': post, 'latestPosts':latestPosts})
